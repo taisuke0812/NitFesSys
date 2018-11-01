cadaab6ee1019387a47d0b9ac63cdba40567c655
fujikawa-t <f.taisuke.numazu@gmail.com>
2018-11-02 01:04:09 +0900

    I cannnot update to determine some access in same time in several apps.

--- app/src/main/java/nit/fes/system/button_activity.java
+++ app/src/main/java/nit/fes/system/button_activity.java
@@ -3,9 +3,13 @@ package nit.fes.system;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,6 +19,7 @@ import java.util.Map;
 public class button_activity extends AppCompatActivity {
     private String name;
     private int key;
+    private String __text;//for test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +31,52 @@ public class button_activity extends AppCompatActivity {
             setName(id_name);
         }
 
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(getName() + "/key");
+        myRef.addValueEventListener(new ValueEventListener() {
+            private static final String TAG ="" ;
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                /*
+                data __data = dataSnapshot.getValue(data.class);
+                double i = __data.getCount();
+                setKey((int)i);
+                */
+                Map<String, Object> map = new HashMap<>();
+                map.put("count",dataSnapshot.getValue(Object.class));
+                String get_key = map.get("count").toString();
+                set__text(get_key);
+                //setKey(Integer.valueOf(get_key));
+                //Log.d(TAG, "Value is: " + get_key);
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+        /*
         key_intent = i.getIntExtra("KEY",0);
         setKey(key_intent);
-
+        */
         Date date = new Date();
         String date_data = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
+
         data send_data = new data(1,"[" + date_data + "]");
         setKey(getKey() + 1);
+        data send_key_count = new data(getKey(),date_data);
+        data send_test = new data (1,get__text());
         sendData(send_data);
+        sendData_(send_key_count);
+        sendData__(send_test);
 
         Intent intent = new Intent(getApplication(), show_image.class);
         //intent.putExtra("DATA", barcodeResult.getText());
@@ -45,6 +88,21 @@ public class button_activity extends AppCompatActivity {
     public void sendData(data _data) {
         DatabaseReference dataref = FirebaseDatabase.getInstance().getReference(getName());
         String key = String.valueOf(this.getKey());
+        Map<String, Object> map = new HashMap<>();
+        map.put(key, _data.toMap());
+        dataref.updateChildren(map);
+    }
+    public void sendData_(data _data) {
+        DatabaseReference dataref = FirebaseDatabase.getInstance().getReference(getName());
+        String key = "key";
+        Map<String, Object> map = new HashMap<>();
+        map.put(key, _data.toMap());
+        dataref.updateChildren(map);
+    }
+
+    public void sendData__(data _data) {
+        DatabaseReference dataref = FirebaseDatabase.getInstance().getReference(getName());
+        String key = "test";
         Map<String, Object> map = new HashMap<>();
         map.put(key, _data.toMap());
         dataref.updateChildren(map);
@@ -61,6 +119,6 @@ public class button_activity extends AppCompatActivity {
     public int  getKey(){
         return this.key;
     }
-
-
+    public String get__text(){return this.__text;}
+    public void set__text(String text){this.__text = text;}
 }
